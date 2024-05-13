e2732ea1758b897ceeb77593677a0ed9cd832332
CodinGlitch <codinglitchdev@gmail.com>
2024-05-13 06:28:59 -0700

    Added microphone block

---
+++ common/src/main/java/com/codinglitch/simpleradio/core/registry/blocks/MicrophoneBlock.java
@@ -0,0 +1,112 @@
+package com.codinglitch.simpleradio.core.registry.blocks;
+
+import com.codinglitch.simpleradio.core.registry.SimpleRadioBlockEntities;
+import net.minecraft.core.BlockPos;
+import net.minecraft.world.entity.LivingEntity;
+import net.minecraft.world.item.ItemStack;
+import net.minecraft.world.item.context.BlockPlaceContext;
+import net.minecraft.world.level.BlockGetter;
+import net.minecraft.world.level.Level;
+import net.minecraft.world.level.block.BaseEntityBlock;
+import net.minecraft.world.level.block.Block;
+import net.minecraft.world.level.block.Mirror;
+import net.minecraft.world.level.block.Rotation;
+import net.minecraft.world.level.block.entity.BlockEntity;
+import net.minecraft.world.level.block.entity.BlockEntityTicker;
+import net.minecraft.world.level.block.entity.BlockEntityType;
+import net.minecraft.world.level.block.state.BlockState;
+import net.minecraft.world.level.block.state.StateDefinition;
+import net.minecraft.world.level.block.state.properties.BlockStateProperties;
+import net.minecraft.world.level.block.state.properties.IntegerProperty;
+import net.minecraft.world.level.block.state.properties.RotationSegment;
+import net.minecraft.world.level.storage.loot.LootParams;
+import net.minecraft.world.level.storage.loot.parameters.LootContextParams;
+import net.minecraft.world.phys.shapes.CollisionContext;
+import net.minecraft.world.phys.shapes.VoxelShape;
+import org.jetbrains.annotations.Nullable;
+
+import java.util.List;
+
+public class MicrophoneBlock extends BaseEntityBlock {
+    public static final int MAX_ROTATION_INDEX = RotationSegment.getMaxSegmentIndex();
+    private static final int MAX_ROTATIONS = MAX_ROTATION_INDEX + 1;
+    public static final IntegerProperty ROTATION = BlockStateProperties.ROTATION_16;
+
+    private static final VoxelShape SHAPE = Block.box(4.0, 0.0, 4.0, 12.0, 14.0, 12.0);
+
+    public MicrophoneBlock(Properties properties) {
+        super(properties);
+        this.registerDefaultState(this.defaultBlockState().setValue(ROTATION, 0));
+    }
+
+    public float getYRotationDegrees(BlockState state) {
+        return RotationSegment.convertToDegrees(state.getValue(ROTATION));
+    }
+
+    @Override
+    protected void createBlockStateDefinition(StateDefinition.Builder<Block, BlockState> stateBuilder) {
+        super.createBlockStateDefinition(stateBuilder.add(ROTATION));
+    }
+
+    @Override
+    public BlockState getStateForPlacement(BlockPlaceContext context) {
+        return this.defaultBlockState()
+                .setValue(ROTATION, RotationSegment.convertToSegment(context.getRotation() + 180.0F));
+    }
+
+    @Override
+    public VoxelShape getShape(BlockState state, BlockGetter getter, BlockPos pos, CollisionContext context) {
+        return SHAPE;
+    }
+
+    @Override
+    public BlockState rotate(BlockState state, Rotation rotation) {
+        return state.setValue(ROTATION, rotation.rotate(state.getValue(ROTATION), MAX_ROTATIONS));
+    }
+
+    @Override
+    public BlockState mirror(BlockState state, Mirror mirror) {
+        return state.setValue(ROTATION, mirror.mirror(state.getValue(ROTATION), MAX_ROTATIONS));
+    }
+
+    @Override
+    public boolean hasAnalogOutputSignal(BlockState $$0) {
+        return true;
+    }
+
+    @Override
+    public int getAnalogOutputSignal(BlockState state, Level level, BlockPos pos) {
+        return 0;
+    }
+
+    public List<ItemStack> getDrops(BlockState state, LootParams.Builder builder) {
+        ItemStack stack = new ItemStack(this);
+        BlockEntity blockEntity = builder.getOptionalParameter(LootContextParams.BLOCK_ENTITY);
+        if (blockEntity instanceof RadioBlockEntity radioBlockEntity)
+            radioBlockEntity.saveToItem(stack);
+
+        return List.of(stack);
+    }
+
+    @Override
+    public void setPlacedBy(Level level, BlockPos blockPos, BlockState blockState, @Nullable LivingEntity entity, ItemStack stack) {
+        BlockEntity blockEntity = level.getBlockEntity(blockPos);
+        if (blockEntity instanceof MicrophoneBlockEntity microphoneBlockEntity) {
+            microphoneBlockEntity.loadFromItem(stack);
+        }
+
+        super.setPlacedBy(level, blockPos, blockState, entity, stack);
+    }
+
+    @Nullable
+    @Override
+    public BlockEntity newBlockEntity(BlockPos pos, BlockState state) {
+        return new MicrophoneBlockEntity(pos, state);
+    }
+
+    @Nullable
+    @Override
+    public <T extends BlockEntity> BlockEntityTicker<T> getTicker(Level level, BlockState state, BlockEntityType<T> type) {
+        return createTickerHelper(type, SimpleRadioBlockEntities.MICROPHONE, MicrophoneBlockEntity::tick);
+    }
+}
---
+++ common/src/main/java/com/codinglitch/simpleradio/core/registry/blocks/MicrophoneBlockEntity.java
@@ -0,0 +1,117 @@
+package com.codinglitch.simpleradio.core.registry.blocks;
+
+import com.codinglitch.simpleradio.core.central.*;
+import com.codinglitch.simpleradio.core.registry.SimpleRadioBlockEntities;
+import com.codinglitch.simpleradio.core.registry.SimpleRadioSounds;
+import com.codinglitch.simpleradio.radio.RadioChannel;
+import com.codinglitch.simpleradio.radio.RadioListener;
+import com.codinglitch.simpleradio.radio.RadioManager;
+import com.codinglitch.simpleradio.radio.RadioSource;
+import net.minecraft.core.BlockPos;
+import net.minecraft.nbt.CompoundTag;
+import net.minecraft.server.level.ServerLevel;
+import net.minecraft.sounds.SoundSource;
+import net.minecraft.world.item.ItemStack;
+import net.minecraft.world.level.Level;
+import net.minecraft.world.level.block.state.BlockState;
+
+import java.util.UUID;
+
+public class MicrophoneBlockEntity extends FrequencyBlockEntity implements Transmitting {
+    public boolean isListening = false;
+    public UUID listenerID;
+
+    public MicrophoneBlockEntity(BlockPos pos, BlockState state) {
+        super(SimpleRadioBlockEntities.MICROPHONE, pos, state);
+
+        this.listenerID = UUID.randomUUID();
+    }
+
+    @Override
+    public void setRemoved() {
+        if (level != null) {
+            level.playSound(
+                    null, this.worldPosition,
+                    SimpleRadioSounds.RADIO_CLOSE,
+                    SoundSource.PLAYERS,
+                    1f,1f
+            );
+        }
+
+
+        if (this.frequency != null)
+            stopListening(WorldlyPosition.of(getBlockPos(), level));
+        super.setRemoved();
+    }
+
+    @Override
+    public void load(CompoundTag tag) {
+        super.load(tag);
+        loadTag(tag);
+    }
+
+    @Override
+    public void saveAdditional(CompoundTag tag) {
+        saveTag(tag);
+        super.saveAdditional(tag);
+    }
+
+    @Override
+    public void saveToItem(ItemStack stack) {
+        saveTag(stack.getOrCreateTag());
+        super.saveToItem(stack);
+    }
+
+    public static void tick(Level level, BlockPos pos, BlockState blockState, MicrophoneBlockEntity blockEntity) {
+        if (!level.isClientSide) {
+            if (blockEntity.frequency != null && !blockEntity.isListening) {
+                blockEntity.listen();
+            }
+        }
+    }
+
+    public void listen() {
+        RadioListener listener = startListening(WorldlyPosition.of(getBlockPos(), level));
+        listener.range = 12;
+        listener.acceptor(source -> {
+            source.type = RadioSource.Type.TRANSCEIVER;
+            source.owner = listenerID;
+
+            Frequency frequency = getFrequency(this);
+            if (frequency != null) RadioManager.transmit(source, frequency);
+        });
+
+        this.frequency.tryAddTransmitter(listener);
+
+        level.playSound(
+                null, this.worldPosition,
+                SimpleRadioSounds.RADIO_OPEN,
+                SoundSource.PLAYERS,
+                1f,1f
+        );
+
+        this.isListening = true;
+    }
+
+    public void loadFromItem(ItemStack stack) {
+        loadTag(stack.getOrCreateTag());
+    }
+
+    public void loadTag(CompoundTag tag) {
+        if (this.frequency != null) {
+            stopListening(WorldlyPosition.of(getBlockPos(), level));
+            this.isListening = false;
+        }
+
+        String frequencyName = tag.getString("frequency");
+        Frequency.Modulation modulation = Frequency.modulationOf(tag.getString("modulation"));
+        this.frequency = Frequency.getOrCreateFrequency(frequencyName, modulation);
+    }
+
+    public void saveTag(CompoundTag tag) {
+        if (this.frequency == null) return;
+
+        tag.putString("frequency", this.frequency.frequency);
+        tag.putString("modulation", this.frequency.modulation.shorthand);
+    }
+}
