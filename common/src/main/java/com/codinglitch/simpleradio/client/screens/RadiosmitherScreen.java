dec4118f4be98e563a1ab102040478331a2b3089
CodinGlitch <codinglitchdev@gmail.com>
2024-05-13 07:23:06 -0700

    Fixed transmitters not showing in radiosmither

--- common/src/main/java/com/codinglitch/simpleradio/client/screens/RadiosmitherScreen.java
+++ common/src/main/java/com/codinglitch/simpleradio/client/screens/RadiosmitherScreen.java
@@ -2,6 +2,7 @@ package com.codinglitch.simpleradio.client.screens;
 
 import com.codinglitch.simpleradio.CommonSimpleRadio;
 import com.codinglitch.simpleradio.client.central.BaseButton;
+import com.codinglitch.simpleradio.core.central.Frequencing;
 import com.codinglitch.simpleradio.core.central.Receiving;
 import com.codinglitch.simpleradio.core.networking.packets.ServerboundRadioUpdatePacket;
 import com.codinglitch.simpleradio.core.registry.menus.RadiosmitherMenu;
@@ -75,7 +76,7 @@ public class RadiosmitherScreen extends AbstractContainerScreen<RadiosmitherMenu
         if (!frequency.isEmpty()) return;
 
         ItemStack tinkering = menu.getTinkering();
-        if (tinkering != null && tinkering.getItem() instanceof Receiving) {
+        if (tinkering != null && tinkering.getItem() instanceof Frequencing) {
             CompoundTag tag = tinkering.getOrCreateTag();
             frequency = tag.getString("frequency");
             modulation = Frequency.modulationOf(tag.getString("modulation"));
@@ -127,7 +128,7 @@ public class RadiosmitherScreen extends AbstractContainerScreen<RadiosmitherMenu
         super.renderLabels(graphics, mouseX, mouseY);
 
         ItemStack tinkering = this.menu.getTinkering();
-        if (tinkering != null && tinkering.getItem() instanceof Receiving) {
+        if (tinkering != null && tinkering.getItem() instanceof Frequencing) {
             if (!frequency.isEmpty() && modulation != null) {
                 graphics.drawString(this.font,
                         Component.literal(frequency)
--- common/src/main/java/com/codinglitch/simpleradio/core/registry/menus/RadiosmitherMenu.java
+++ common/src/main/java/com/codinglitch/simpleradio/core/registry/menus/RadiosmitherMenu.java
@@ -1,7 +1,7 @@
 package com.codinglitch.simpleradio.core.registry.menus;
 
+import com.codinglitch.simpleradio.core.central.Frequencing;
 import com.codinglitch.simpleradio.core.central.Frequency;
-import com.codinglitch.simpleradio.core.central.Receiving;
 import com.codinglitch.simpleradio.core.registry.SimpleRadioMenus;
 import net.minecraft.nbt.CompoundTag;
 import net.minecraft.world.Container;
@@ -42,8 +42,8 @@ public class RadiosmitherMenu extends AbstractContainerMenu {
 
     public void updateTinkering(String frequency, Frequency.Modulation modulation) {
         ItemStack tinkering = this.getTinkering();
-        if (!tinkering.isEmpty() && tinkering.getItem() instanceof Receiving receiving) {
-            receiving.setFrequency(tinkering, frequency, modulation);
+        if (!tinkering.isEmpty() && tinkering.getItem() instanceof Frequencing frequencing) {
+            frequencing.setFrequency(tinkering, frequency, modulation);
 
             CompoundTag tag = tinkering.getOrCreateTag();
             if (tag.contains("user")) {
