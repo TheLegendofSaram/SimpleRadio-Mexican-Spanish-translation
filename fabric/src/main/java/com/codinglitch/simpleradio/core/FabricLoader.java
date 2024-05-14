d08b754cff069763bff50d849a8292bbe59c9e2a
CodinGlitch <codinglitchdev@gmail.com>
2024-05-13 23:58:20 -0700

    Item registration restructure

--- fabric/src/main/java/com/codinglitch/simpleradio/core/FabricLoader.java
+++ fabric/src/main/java/com/codinglitch/simpleradio/core/FabricLoader.java
@@ -1,18 +1,29 @@
 package com.codinglitch.simpleradio.core;
 
+import com.codinglitch.simpleradio.CommonSimpleRadio;
+import com.codinglitch.simpleradio.core.central.ItemHolder;
 import com.codinglitch.simpleradio.core.networking.packets.ClientboundRadioPacket;
 import com.codinglitch.simpleradio.core.networking.packets.ServerboundRadioUpdatePacket;
 import com.codinglitch.simpleradio.core.registry.SimpleRadioBlockEntities;
 import com.codinglitch.simpleradio.core.registry.SimpleRadioBlocks;
 import com.codinglitch.simpleradio.core.registry.SimpleRadioItems;
 import com.codinglitch.simpleradio.core.registry.SimpleRadioMenus;
+import com.google.gson.JsonArray;
+import com.google.gson.JsonElement;
+import com.google.gson.JsonObject;
+import com.google.gson.JsonParseException;
 import net.fabricmc.fabric.api.client.networking.v1.ClientPlayNetworking;
 import net.fabricmc.fabric.api.networking.v1.ServerPlayNetworking;
+import net.fabricmc.fabric.api.resource.conditions.v1.ConditionJsonProvider;
+import net.fabricmc.fabric.api.resource.conditions.v1.ResourceConditions;
 import net.minecraft.core.Registry;
 import net.minecraft.core.registries.BuiltInRegistries;
 import net.minecraft.network.FriendlyByteBuf;
+import net.minecraft.resources.ResourceLocation;
 import net.minecraft.server.MinecraftServer;
 import net.minecraft.server.level.ServerPlayer;
+import net.minecraft.util.GsonHelper;
+import net.minecraft.world.item.Item;
 import org.apache.logging.log4j.util.TriConsumer;
 
 import java.util.function.Consumer;
@@ -20,7 +31,15 @@ import java.util.function.Function;
 
 public class FabricLoader {
     public static void loadItems() {
-        SimpleRadioItems.ITEMS.forEach(((location, item) -> Registry.register(BuiltInRegistries.ITEM, location, item)));
+        SimpleRadioItems.ITEMS.forEach(((location, item) -> {
+            ResourceLocation conditionLocation = location.withSuffix("_enabled");
+
+            ResourceConditions.register(conditionLocation, object -> {
+                return item.enabled; // i have no idea if this is how youre supposed to do it
+            });
+
+            Registry.register(BuiltInRegistries.ITEM, location, item.get());
+        }));
     }
 
     public static void loadBlocks() {
@@ -42,6 +61,43 @@ public class FabricLoader {
     }
     public static <P> ClientPlayNetworking.PlayChannelHandler clientbound(Function<FriendlyByteBuf, P> decoder, Consumer<P> consumer) {
         return (client, listener, buffer, sender) -> consumer.accept(decoder.apply(buffer));
+    }
+
+    private static final ResourceLocation ITEMS_ENABLED = CommonSimpleRadio.id("items_enabled");
+
+    public static ConditionJsonProvider itemsEnabled(String... items) {
+        return new ConditionJsonProvider() {
+            @Override
+            public void writeParameters(JsonObject object) {
+                JsonArray array = new JsonArray();
+                for (var item : items) {
+                    array.add(item);
+                }
+                object.add("values", array);
+            }
+
+            @Override
+            public ResourceLocation getConditionId() {
+                return ITEMS_ENABLED;
+            }
+        };
+    }
+
+    static {
+        ResourceConditions.register(ITEMS_ENABLED, object -> {
+            JsonArray array = GsonHelper.getAsJsonArray(object, "values");
+
+            for (JsonElement element : array) {
+                if (element.isJsonPrimitive()) {
+                    ItemHolder<Item> holder = SimpleRadioItems.getByName(element.getAsString());
+                    if (holder != null) return holder.enabled;
+                } else {
+                    throw new JsonParseException("Invalid item entry: " + element);
+                }
+            }
+
+            return true;
+        });
     }
 
     public static void load() {
