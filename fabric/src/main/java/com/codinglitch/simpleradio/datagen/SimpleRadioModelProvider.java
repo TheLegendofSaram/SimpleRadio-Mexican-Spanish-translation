bd1ebbb33fe977716cb96e9c84978fc4ea65d4e9
CodinGlitch <codinglitchdev@gmail.com>
2024-06-11 00:23:31 -0700

    Cleared old code

--- fabric/src/main/java/com/codinglitch/simpleradio/datagen/SimpleRadioModelProvider.java
+++ fabric/src/main/java/com/codinglitch/simpleradio/datagen/SimpleRadioModelProvider.java
@@ -1,16 +1,11 @@
 package com.codinglitch.simpleradio.datagen;
 
-import com.codinglitch.simpleradio.CommonSimpleRadio;
-import com.codinglitch.simpleradio.core.registry.SimpleRadioBlocks;
 import com.codinglitch.simpleradio.core.registry.SimpleRadioItems;
-import com.codinglitch.simpleradio.core.registry.items.UpgradeModuleItem;
+import com.codinglitch.simpleradio.core.registry.items.ModuleItem;
 import net.fabricmc.fabric.api.datagen.v1.FabricDataOutput;
-import net.fabricmc.fabric.api.datagen.v1.provider.FabricBlockLootTableProvider;
 import net.fabricmc.fabric.api.datagen.v1.provider.FabricModelProvider;
-import net.minecraft.client.renderer.block.model.ItemModelGenerator;
 import net.minecraft.data.models.BlockModelGenerators;
 import net.minecraft.data.models.ItemModelGenerators;
-import net.minecraft.resources.ResourceLocation;
 
 public class SimpleRadioModelProvider extends FabricModelProvider {
     public SimpleRadioModelProvider(FabricDataOutput generator) {
@@ -24,13 +19,6 @@ public class SimpleRadioModelProvider extends FabricModelProvider {
 
     @Override
     public void generateItemModels(ItemModelGenerators itemModelGenerator) {
-        SimpleRadioItems.ITEMS.forEach((location, itemHolder) -> {
-            if (itemHolder.get() instanceof UpgradeModuleItem upgradeModuleItem) {
-                /*itemModelGenerator.generateLayeredItem(location.withPrefix("item/"),
-                        CommonSimpleRadio.id("item/", upgradeModuleItem.getTier().toString().toLowerCase(), "_upgrade_module"),
-                        CommonSimpleRadio.id("item/", upgradeModuleItem.getType().toString().toLowerCase(), "_upgrade")
-                );*/
-            }
-        });
+
     }
 }
