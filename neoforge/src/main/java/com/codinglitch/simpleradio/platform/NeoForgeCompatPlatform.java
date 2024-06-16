ce37059423abbcf54234e123d44617ee0f1f25f6
CodinGlitch <codinglitchdev@gmail.com>
2024-06-16 15:12:54 -0700

    Adapted to 1.20.2

--- neoforge/src/main/java/com/codinglitch/simpleradio/platform/NeoForgeCompatPlatform.java
+++ neoforge/src/main/java/com/codinglitch/simpleradio/platform/NeoForgeCompatPlatform.java
@@ -1,15 +1,11 @@
 package com.codinglitch.simpleradio.platform;
 
+import com.codinglitch.simpleradio.CompatCore;
+import com.codinglitch.simpleradio.compat.ValkyrienCompat;
 import com.codinglitch.simpleradio.core.central.WorldlyPosition;
 import com.codinglitch.simpleradio.platform.services.CompatPlatform;
 import com.codinglitch.simpleradio.radio.RadioChannel;
 import com.codinglitch.simpleradio.radio.RadioSource;
-import net.minecraft.core.BlockPos;
-import net.minecraft.server.level.ServerLevel;
-import net.minecraft.world.level.Level;
-import org.joml.Matrix4dc;
-import org.joml.Vector3d;
-import org.joml.Vector3f;
 
 public class NeoForgeCompatPlatform implements CompatPlatform {
     @Override
@@ -18,12 +14,13 @@ public class NeoForgeCompatPlatform implements CompatPlatform {
     }
 
     @Override
-    public WorldlyPosition modifyPosition(BlockPos originalBlockPos, Level level) {
-        return WorldlyPosition.of(originalBlockPos, level);
-    }
+    public WorldlyPosition modifyPosition(WorldlyPosition position) {
 
-    @Override
-    public Vector3f modifyPosition(Level level, BlockPos originalBlockPos) {
-        return new Vector3f(originalBlockPos.getX() + 0.5F, originalBlockPos.getY() + 0.5F, originalBlockPos.getZ() + 0.5F);
+        // ---- Valkyrien Skies ---- \\
+        if (CompatCore.VALKYRIEN_SKIES) {
+            return ValkyrienCompat.modifyPosition(position);
+        }
+
+        return position;
     }
 }
