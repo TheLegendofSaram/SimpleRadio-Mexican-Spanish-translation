8f2258cedd8fe1f1af26c9960a37880d37b5febf
CodinGlitch <codinglitchdev@gmail.com>
2024-06-20 15:19:36 -0700

    Fixed radiosmither container

--- common/src/main/java/com/codinglitch/simpleradio/core/registry/blocks/RadiosmitherBlockEntity.java
+++ common/src/main/java/com/codinglitch/simpleradio/core/registry/blocks/RadiosmitherBlockEntity.java
@@ -7,6 +7,7 @@ import net.minecraft.core.BlockPos;
 import net.minecraft.core.NonNullList;
 import net.minecraft.nbt.CompoundTag;
 import net.minecraft.network.chat.Component;
+import net.minecraft.world.Container;
 import net.minecraft.world.ContainerHelper;
 import net.minecraft.world.MenuProvider;
 import net.minecraft.world.entity.player.Inventory;
@@ -14,12 +15,15 @@ import net.minecraft.world.entity.player.Player;
 import net.minecraft.world.inventory.AbstractContainerMenu;
 import net.minecraft.world.item.ItemStack;
 import net.minecraft.world.level.Level;
+import net.minecraft.world.level.block.entity.BaseContainerBlockEntity;
 import net.minecraft.world.level.block.entity.BlockEntity;
 import net.minecraft.world.level.block.state.BlockState;
 import org.jetbrains.annotations.Nullable;
 
-public class RadiosmitherBlockEntity extends BlockEntity implements MenuProvider, BaseContainer {
-    private final NonNullList<ItemStack> items = NonNullList.withSize(2, ItemStack.EMPTY);
+public class RadiosmitherBlockEntity extends BaseContainerBlockEntity {
+    public static final int CONTAINER_SIZE = 1;
+
+    private final NonNullList<ItemStack> items = NonNullList.withSize(CONTAINER_SIZE, ItemStack.EMPTY);
 
     public RadiosmitherBlockEntity(BlockPos pos, BlockState state) {
         super(SimpleRadioBlockEntities.RADIOSMITHER, pos, state);
@@ -30,8 +34,51 @@ public class RadiosmitherBlockEntity extends BlockEntity implements MenuProvider
     }
 
     @Override
+    public int getContainerSize() {
+        return CONTAINER_SIZE;
+    }
+
+    @Override
+    public boolean isEmpty() {
+        return items.isEmpty();
+    }
+
+    @Override
+    public ItemStack getItem(int i) {
+        return items.get(i);
+    }
+
+    @Override
+    public ItemStack removeItem(int i, int count) {
+        ItemStack stack = items.get(i);
+        ItemStack copy = stack.copyWithCount(count);
+        stack.shrink(count);
+        return copy;
+    }
+
+    @Override
+    public ItemStack removeItemNoUpdate(int i) {
+        return items.get(i).copyAndClear();
+    }
+
+    @Override
+    public void setItem(int i, ItemStack itemStack) {
+        items.set(i, itemStack);
+    }
+
+    @Override
+    public boolean stillValid(Player player) {
+        return Container.stillValidBlockEntity(this, player);
+    }
+
+    @Override
     public Component getDisplayName() {
         return Component.translatable(getBlockState().getBlock().getDescriptionId());
+    }
+
+    @Override
+    protected Component getDefaultName() {
+        return null;
     }
 
     @Nullable
@@ -40,17 +87,17 @@ public class RadiosmitherBlockEntity extends BlockEntity implements MenuProvider
         return new RadiosmitherMenu(i, inventory, this);
     }
 
+    @Override
+    protected AbstractContainerMenu createMenu(int i, Inventory inventory) {
+        return null;
+    }
+
     public ItemStack tinkering() {
         return items.get(0);
     }
 
     public ItemStack applying() {
         return items.get(1);
-    }
-
-    @Override
-    public NonNullList<ItemStack> getItems() {
-        return items;
     }
 
     @Override
@@ -63,5 +110,10 @@ public class RadiosmitherBlockEntity extends BlockEntity implements MenuProvider
     public void saveAdditional(CompoundTag tag) {
         ContainerHelper.saveAllItems(tag, items);
         super.saveAdditional(tag);
+    }
+
+    @Override
+    public void clearContent() {
+
     }
 }
